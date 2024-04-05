b5125f7d432ff2a2bd673d6e8935f8ab0840653f
vvzqcx <158055027+vvzqcx@users.noreply.github.com>
2024-04-05 12:43:42 +0800

    Update tb.sv

--- tb.sv
+++ tb.sv
@@ -1,24 +1,36 @@
-`timescale 1ns / 1ps
+`timescale 1ns / 1ns
 
 module tb();
-// input clk,reset;
-// input[1:0]  workstate;//開始給資料就變成1 運算結束變0
-// input[24:0] penumx,penumy,sizex,sizey,kernelsize;
-// output[24:0] tsv_out;
-integer input_totalnum;
 reg clk,rst;
-integer kernelsize[1:0];
-//reg [24:0] penumx,penumy,sizex,sizey,kernelsize;
-logic[41:0]tsv_out[8:0][8:0];
-Controller inst(.clk(clk), .rst(rst), .kernelsize(kernelsize), .tsv_out(tsv_out), .input_totalnum(input_totalnum));
-//, .penumx(penumx), .penumy(penumy), .sizex(sizex), .sizey(sizey), .kernelsize(kernelsize)
+
+//input_totalnum指的是組共有多少運算需要被執行
+//total_inputmapnum指的是一條input map有多少運算
+//例如這邊input_totalnum有1000代表總共有五輪的weight可以運算
+integer input_totalnum,total_inputmapnum;
+logic [5:0] kernelsize;
+logic same;
+logic [18:0] OSIS[15:0][15:0];
+logic [18:0] OSWS[15:0][15:0];
+logic [18:0] WSIS[15:0][15:0];
+logic [18:0] WSWS[15:0][15:0];
+
+Controller1 inst(.clk(clk), .rst(rst), .kernelsize(kernelsize), .same(same), .OSIS(OSIS), .OSWS(OSWS), .WSIS(WSIS), .WSWS(WSWS), .input_totalnum(input_totalnum), .total_inputmapnum(total_inputmapnum));
 
 initial begin 
+    for(int a=0; a<=15 ; a=a+1)begin
+        for(int b=0 ; b<=15 ; b=b+1)begin
+            OSIS[a][b] = 1;
+            OSWS[a][b] = 1;
+            WSIS[a][b] = 1;
+            WSWS[a][b] = 1;
+        end
+    end
     clk=1;
     rst=1;
-    kernelsize[0] = 2;
-    kernelsize[1] = 2;
-    input_totalnum = 200;
+    same=1;
+    kernelsize = 2;
+    input_totalnum = 1000;
+    total_inputmapnum = 200;
     #10 rst=0;
     #1500 $finish;
 end
